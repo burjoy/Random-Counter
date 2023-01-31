f803f0f6383c4e73984ecf20f43f4607ce35e0ca
Luqman <51292479+burjoy@users.noreply.github.com>
2023-01-31 11:12:00 +0700

    Update script.js
    Add level up system

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let tebakan = document.getElementById("tebakan");
 let butt = document.getElementById("butts");
 let progress = document.getElementById("progress");
+let level = document.getElementById("level");
 let benar = 0;
+let no_level = 1;
 butt.addEventListener("click", function(){
 	let nilai = tebakan.value;
 	cek = Math.round(Math.random()*5)
@@ -16,6 +18,14 @@ butt.addEventListener("click", function(){
 			progress.classList.add('h-full');
 			progress.classList.remove(regres);
 		}
+		if(benar == 5){
+			no_level++;
+			benar = 0;
+			level.innerText = 'Level: ' + no_level;
+			progress.classList.remove('w-[100%]');
+			progress.classList.remove('bg-sky-500');
+		}
+		console.log(benar);
 	}
 	else{
 		console.log('Salah!');
