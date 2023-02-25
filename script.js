f45dff45b6e05d34dc883394007ab2319fb94807
Luqman <51292479+burjoy@users.noreply.github.com>
2023-02-26 06:47:39 +0700

    Nambah mechanic buat sistem life

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ let butt = document.getElementById("butts");
 let progress = document.getElementById("progress");
 let level = document.getElementById("level");
 let benar = 0;
+let salah = 5;
 let no_level = 1;
-let cek = Math.round(Math.random()*5);
-butt.addEventListener("click", function(){
+let cek = Math.floor((Math.random()*5)+1);
+let color_slide = document.querySelectorAll('div#slider_warna span');
+let sliders = document.querySelectorAll('input[type = range]');
+let life = document.getElementById('life');
+
+let ganti_warna = () => {
+	progress.style.backgroundColor = `rgb(${sliders[0].value}, ${sliders[1].value}, ${sliders[2].value})`;
+	for(x = 0;x < color_slide.length;x++){
+		color_slide[x].innerText = sliders[x].value;
+	}
+}
+
+for(x of sliders){
+	x.addEventListener('input', ganti_warna);
+}
+
+let cons = () => {
 	let nilai = tebakan.value;
 	if(nilai == cek){
-		cek = Math.round(Math.random()*5);
+		cek = Math.floor((Math.random()* 5))+1;
 		if(benar < 5){
 			benar++;
 			let proses = 'w-'+'['+benar/5*100+'%'+']';
@@ -27,13 +43,22 @@ butt.addEventListener("click", function(){
 			level.innerText = 'Level: ' + no_level;
 			progress.classList.remove('w-[100%]');
 			progress.classList.remove('bg-sky-500');
+			salah = 0;
 		}
 	}
 	else{
+		life.classList.remove(`w-[${salah/5*100}%]`);
+		salah--;
 		console.log('Salah!');
 		console.log(cek);
+		life.classList.add(`w-[${salah/5*100}%]`);
 	}
-});
+	if(nilai == ''){
+		alert('Mohon isi kotak jawaban');
+	}
+}
+butt.addEventListener("click", cons);
+
 // document.addEventListener('click', function(e){
 // 	console.log(e.target);
-// })
+// });
